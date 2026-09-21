9d0cd9f612ad40d1f33646770a9ccbe52127f635
pedromst2000 <pedromst2000@gmail.com>
2026-09-21 14:07:17 +0100

    refactor: Add Padding and margin reusable CSS responsive styles

---
+++ webapp/src/utils/responsive.jsx
@@ -0,0 +1,51 @@
+/**
+ * @function getPaddingClasses
+ * @description Responsive utility function for determining padding classes based on window width.
+ * @param {Object} windowDimension - The window dimension object containing the width.
+ * @returns {string} The appropriate padding class based on the window width.
+ */
+
+export const getPaddingClasses = (windowDimension) => {
+  const w = windowDimension.width;
+  if (w <= 360) {
+    return "px-[24px]";
+  } else if (w >= 361 && w <= 695) {
+    return "px-[50px]";
+  } else if (w >= 1536 && w <= 1550) {
+    return "px-[74px]";
+  } else if (w >= 1551 && w <= 1579) {
+    return "px-[74px]";
+  } else if (w >= 1580 && w <= 1599) {
+    return "px-[50px]";
+  } else if (w >= 1600 && w <= 1620) {
+    return "px-[50px]";
+  } else if (w >= 1280 && w <= 1380) {
+    return "px-[50px]";
+  } else if (w >= 1024 && w <= 1090) {
+    return "px-[50px]";
+  } else if (w >= 768 && w <= 825) {
+    return "px-[35px]";
+  } else if (w >= 1091) {
+    return "px-[24px]";
+  } else {
+    return "px-0";
+  }
+};
+
+/**
+ * @function getMarginClasses
+ * @description Responsive utility function for determining margin classes based on window width.
+ * @param {Object} windowDimension - The window dimension object containing the width.
+ * @returns {string} The appropriate margin class based on the window width.
+ */
+
+export const getMarginClasses = (windowDimension) => {
+  const w = windowDimension.width;
+  if (w < 640) {
+    return "my-[46px]";
+  } else if (w < 1024) {
+    return "my-[62px]";
+  } else {
+    return "my-[72px]";
+  }
+};
